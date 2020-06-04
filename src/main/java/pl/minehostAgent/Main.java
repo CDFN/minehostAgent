d27dde8f9614e9ebf9877880c8d1623e414c0431
CDFN <codefun@protonmail.com>
2020-06-04 13:31:22 +0200

    Refactor code

--- src/main/java/pl/minehostAgent/Main.java
+++ src/main/java/pl/minehostAgent/Main.java
@@ -4,24 +4,24 @@ package pl.minehostAgent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
-import pl.minehostAgent.data.DataLoader;
+import pl.minehostAgent.manager.DataManager;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onLoad() {
-        if (Bukkit.getServer().getVersion().contains("1.15") || Bukkit.getServer().getVersion().contains("1.14")) {
-            DataLoader dataLoader = new DataLoader();
-            dataLoader.load();
-
-            super.onLoad();
-        } else {
+        if (!Bukkit.getServer().getVersion().contains("1.15") && !Bukkit.getServer().getVersion().contains("1.14")) {
             System.out.println(ChatColor.RED + " Wersja " + Bukkit.getServer().getVersion() + " nie jest wspierana!");
+            return;
         }
+        this.saveDefaultSettings();
+        DataManager dataManager = new DataManager(this, "bukkit.yml", "spigot.yml", "paper.yml");
+        dataManager.loadDataFromConfig();
     }
 
-    @Override
-    public void onEnable() {
-        super.onEnable();
+    private void saveDefaultSettings() {
+        this.saveResource("bukkit.yml", false);
+        this.saveResource("spigot.yml", false);
+        this.saveResource("paper.yml", false);
     }
 }
---
+++ src/main/java/pl/minehostAgent/manager/DataManager.java
@@ -0,0 +1,52 @@
+package pl.minehostAgent.manager;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import pl.minehostAgent.Main;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+public class DataManager {
+    private final Main plugin;
+    private final Map<String, FileConfiguration> serversConfiguration = new HashMap<>();
+    private final Map<String, FileConfiguration> pluginsConfiguration = new HashMap<>();
+
+    public DataManager(Main plugin, String... filesToLoad) {
+        this.plugin = plugin;
+        for (String fileName : filesToLoad) {
+            serversConfiguration.put(fileName, YamlConfiguration.loadConfiguration(new File(fileName)));
+            pluginsConfiguration.put(fileName, YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), fileName)));
+        }
+    }
+
+    public void loadDataFromConfig() {
+        this.pluginsConfiguration.forEach((fileName, config) -> {
+            config.getKeys(true).forEach(key -> serversConfiguration.get(fileName).set(key, config.get(key)));
+
+            try {
+                serversConfiguration.get(fileName).save(fileName);
+            } catch (IOException e) {
+                plugin.getLogger().log(Level.WARNING, "Failed to save " + fileName);
+                e.printStackTrace();
+            }
+        });
+
+        System.out.println();
+        System.out.println(ChatColor.GREEN + "           _      ___                   _   \n");
+        System.out.println(ChatColor.GREEN + "          | |    / _ \\                 | |  \n");
+        System.out.println(ChatColor.GREEN + " _ __ ___ | |__ / /_\\ \\ __ _  ___ _ __ | |_ \n");
+        System.out.println(ChatColor.GREEN + "| '_ ` _ \\| '_ \\|  _  |/ _` |/ _ \\ '_ \\| __|\n");
+        System.out.println(ChatColor.GREEN + "| | | | | | | | | | | | (_| |  __/ | | | |_ \n");
+        System.out.println(ChatColor.GREEN + "|_| |_| |_|_| |_\\_| |_/\\__, |\\___|_| |_|\\__|\n");
+        System.out.println(ChatColor.GREEN + "                        __/ |               \n");
+        System.out.println(ChatColor.GREEN + "                       |___/                ");
+        System.out.println();
+        System.out.println(ChatColor.GREEN + " Domyślne ustawienia serwera zalecane przez MINEHOST.PL zostały pomyślnie załadowane.");
+
+    }
+}
